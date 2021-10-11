b39c7cbe232b2675b6fd68be0047119302f7fefa
Kumar Sunil <sunil@smarttersstudio.com>
2021-10-11 15:28:21 +0530

    null safety

--- lib/linked_with_login/data_model/auth_success_response.dart
+++ lib/linked_with_login/data_model/auth_success_response.dart
@@ -4,26 +4,29 @@
 
 import 'dart:convert';
 
-AuthorizationSuccessResponse authorizationSuccessResponseFromJson(String str) => AuthorizationSuccessResponse.fromJson(json.decode(str));
+AuthorizationSuccessResponse authorizationSuccessResponseFromJson(String str) =>
+    AuthorizationSuccessResponse.fromJson(json.decode(str));
 
-String authorizationSuccessResponseToJson(AuthorizationSuccessResponse data) => json.encode(data.toJson());
+String authorizationSuccessResponseToJson(AuthorizationSuccessResponse data) =>
+    json.encode(data.toJson());
 
 class AuthorizationSuccessResponse {
-    AuthorizationSuccessResponse({
-        this.code,
-        this.state,
-    });
-    
-    String code;
-    String state;
-    
-    factory AuthorizationSuccessResponse.fromJson(Map<String, dynamic> json) => AuthorizationSuccessResponse(
+  AuthorizationSuccessResponse({
+    this.code,
+    this.state,
+  });
+
+  String? code;
+  String? state;
+
+  factory AuthorizationSuccessResponse.fromJson(Map<String, dynamic> json) =>
+      AuthorizationSuccessResponse(
         code: json["code"],
         state: json["state"],
-    );
-    
-    Map<String, dynamic> toJson() => {
+      );
+
+  Map<String, dynamic> toJson() => {
         "code": code,
         "state": state,
-    };
+      };
 }
--- lib/linked_with_login/helpers/authorization_helper.dart
+++ lib/linked_with_login/helpers/authorization_helper.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:loginwithlinkedin/linked_with_login/config/api_routes.dart';
+import 'package:flutter_linkedin/linked_with_login/config/api_routes.dart';
 
 ///
 /// Created By Guru (guru@smarttersstudio.com) on 27/06/20 3:39 PM
 ///
 
 String getAuthorizationUrl({
-    @required String clientId ,
-    @required String clientSecret ,
-    @required String redirectUri,
+    required String clientId ,
+    required String clientSecret ,
+    required String redirectUri,
     String responseType = 'code',
     String state = 'DCEeFWf45A53sdfKef424',
     String scope = 'r_liteprofile%20r_emailaddress'
